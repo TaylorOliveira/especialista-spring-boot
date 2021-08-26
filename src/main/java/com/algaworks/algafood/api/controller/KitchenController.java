00fc56387fbd49402089106d861c550c739336b4
Taylor <uaitayloroliveira@gmail.com>
2021-08-26 17:35:26 -0300

    alga4.13 - Especificando o MediaType para o scope da classe

--- src/main/java/com/algaworks/algafood/api/controller/KitchenController.java
+++ src/main/java/com/algaworks/algafood/api/controller/KitchenController.java
@@ -2,14 +2,13 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.domain.model.Kitchen;
 import com.algaworks.algafood.domain.repository.KitchenRepository;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
-@RequestMapping("/kitchens")
+@RequestMapping(value = "/kitchens", produces = MediaType.APPLICATION_JSON_VALUE)
 public class KitchenController {
 
     private final KitchenRepository kitchenRepository;
@@ -18,7 +17,6 @@ public class KitchenController {
         this.kitchenRepository = kitchenRepository;
     }
 
-    @GetMapping
     public List<Kitchen> list() {
         return kitchenRepository.list();
     }

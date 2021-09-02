99382579a08bba5fbbe412cf8b5ade0317cddc46
Taylor <uaitayloroliveira@gmail.com>
2021-09-02 11:02:00 -0300

    alga4.21 - Corrigindo o Status HTTP para resource inexistente

--- src/main/java/com/algaworks/algafood/api/controller/KitchenController.java
+++ src/main/java/com/algaworks/algafood/api/controller/KitchenController.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/kitchens", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
@@ -30,17 +30,12 @@ public class KitchenController {
         return new KitchenXmlResponse(kitchenRepository.list());
     }
 
-    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{kitchenId}")
     public ResponseEntity<Kitchen> search(@PathVariable Long kitchenId) {
-        Kitchen kitchen =  kitchenRepository.search(kitchenId);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add(HttpHeaders.LOCATION, "http://localhost:8080/kitchens");
-        // return ResponseEntity.ok(kitchen);
-        // return ResponseEntity.status(HttpStatus.OK).body(kitchen);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .headers(httpHeaders)
-                .build();
+        Kitchen kitchen = kitchenRepository.search(kitchenId);
+        if(Objects.nonNull(kitchen)) {
+            return ResponseEntity.ok(kitchen);
+        }
+        return ResponseEntity.notFound().build();
     }
 }

dff3e2a2f82692bc39c85f975d4d9e48b21a5ec6
Taylor <uaitayloroliveira@gmail.com>
2021-09-24 15:21:38 -0300

    alga5.06 - Consultas com query methods.

--- src/main/java/com/algaworks/algafood/domain/repository/KitchenRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/KitchenRepository.java
@@ -1,9 +1,12 @@
 package com.algaworks.algafood.domain.repository;
 
-import com.algaworks.algafood.domain.model.Kitchen;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.algaworks.algafood.domain.model.Kitchen;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface KitchenRepository extends JpaRepository<Kitchen, Long> {
+
+    List<Kitchen> name(String name);
 }

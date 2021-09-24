43256d00fa1a67b3b6d01d5459513054a0d0388b
Taylor <uaitayloroliveira@gmail.com>
2021-09-24 15:31:16 -0300

    alga5.07 - Usando as keywords para definir critérios de query methods.

--- src/main/java/com/algaworks/algafood/domain/repository/RestoreRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestoreRepository.java
@@ -3,7 +3,12 @@ package com.algaworks.algafood.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.algaworks.algafood.domain.model.Restore;
 import org.springframework.stereotype.Repository;
+import java.math.BigDecimal;
+import java.util.List;
 
 @Repository
 public interface RestoreRepository extends JpaRepository<Restore, Long> {
+
+    List<Restore> findByShippingFeeBetween(BigDecimal first, BigDecimal last);
+    List<Restore> findByNameContainingAndKitchenId(String name, Long kitchen);
 }

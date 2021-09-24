7ae245257a0cd1890665dac0147af0d0a4435960
Taylor <uaitayloroliveira@gmail.com>
2021-09-24 15:41:44 -0300

    alga5.08 - Prefixos de query methods.

--- src/main/java/com/algaworks/algafood/domain/repository/RestoreRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestoreRepository.java
@@ -4,11 +4,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.algaworks.algafood.domain.model.Restore;
 import org.springframework.stereotype.Repository;
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.List;
 
 @Repository
 public interface RestoreRepository extends JpaRepository<Restore, Long> {
 
     List<Restore> findByShippingFeeBetween(BigDecimal first, BigDecimal last);
+
     List<Restore> findByNameContainingAndKitchenId(String name, Long kitchen);
+
+    Optional<Restore> findFirstByNameContaining(String name);
+
+    List<Restore> findTop2ByNameContaining(String name);
+
+    int countByKitchenId(Long kitchenId);
 }

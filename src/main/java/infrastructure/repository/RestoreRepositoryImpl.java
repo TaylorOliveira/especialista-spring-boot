1398f5ecdf8d133fcc1bfd5f321a27f77e7e25fa
Taylor <uaitayloroliveira@gmail.com>
2021-09-25 21:00:41 -0300

    alga5.12 - Implementando uma consulta dinâmica com JPQL.

--- src/main/java/infrastructure/repository/RestoreRepositoryImpl.java
+++ src/main/java/infrastructure/repository/RestoreRepositoryImpl.java
@@ -1,13 +1,16 @@
 package infrastructure.repository;
 
-import com.algaworks.algafood.domain.model.Restore;
 import com.algaworks.algafood.domain.repository.CustomizeRestoreRepository;
+import com.algaworks.algafood.domain.model.Restore;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
+import org.springframework.util.StringUtils;
 import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class RestoreRepositoryImpl implements CustomizeRestoreRepository {
@@ -17,12 +20,24 @@ public class RestoreRepositoryImpl implements CustomizeRestoreRepository {
 
     public List<Restore> find(String name,
                               BigDecimal startShippingFee, BigDecimal lastShippingFee) {
-        var jpql = "FROM Restore WHERE name LIKE :name and shippingFee " +
-                "between :startShippingFee and :lastShippingFee";
-        return entityManager.createQuery(jpql, Restore.class)
-                .setParameter("name", "%"+name+"%")
-                .setParameter("startShippingFee", startShippingFee)
-                .setParameter("lastShippingFee", lastShippingFee)
-                .getResultList();
+        var jpql = new StringBuilder();
+        var parameters = new HashMap<String, Object>();
+        jpql.append("from Restore where 0 = 0 ");
+        if(StringUtils.hasLength(name)) {
+            jpql.append("and name like :name ");
+            parameters.put("name", "%"+name+"%");
+        }
+        if(Objects.nonNull(startShippingFee)) {
+            jpql.append("and shippingFee >= :startShippingFee ");
+            parameters.put("startShippingFee", startShippingFee);
+        }
+        if(Objects.nonNull(lastShippingFee)) {
+            jpql.append("and shippingFee <= :lastShippingFee");
+            parameters.put("lastShi" +
+                    "ppingFee", lastShippingFee);
+        }
+        TypedQuery<Restore> query = entityManager.createQuery(jpql.toString(), Restore.class);
+        parameters.forEach(query::setParameter);
+        return query.getResultList();
     }
 }

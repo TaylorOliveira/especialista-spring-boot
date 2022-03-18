005c160b36daec3299773bc31078722185b2b063
taylorsantosoliveira@hotmail.com <Suco071952804$>
2022-03-17 21:10:35 -0300

    6.6. Mapeando propriedades com @CreationTimestamp e @UpdateTimestamp.

--- src/main/java/com/algaworks/algafood/domain/model/Restore.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restore.java
@@ -3,8 +3,12 @@ package com.algaworks.algafood.domain.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,4 +40,12 @@ public class Restore {
         joinColumns = @JoinColumn(name = "restore_id"),
         inverseJoinColumns = @JoinColumn(name = "payment_method_id"))
     private List<PaymentMethod> paymentMethodList = new ArrayList<>();
+
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "dateTime")
+    private LocalDateTime creationDateTime;
+
+    @UpdateTimestamp
+    @Column(nullable = false, columnDefinition = "dateTime")
+    private LocalDateTime updatedDateTime;
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
 insert into kitchen (id, name) values (1, 'Thailand');
 insert into kitchen (id, name) values (2, 'Indiana');
 
-insert into restore (id, name, shipping_fee, kitchen_id) values (1, 'Thai Gourmet', 10, 1);
-insert into restore (id, name, shipping_fee, kitchen_id) values (2, 'Thai Delivery', 9.50, 1);
-insert into restore (id, name, shipping_fee, kitchen_id) values (3, 'Tuk Tuk Comida Indiana', 15, 2);
+insert into restore (id, name, shipping_fee, kitchen_id, creation_date_time, updated_date_time) values (1, 'Thai Gourmet', 10, 1, utc_timestamp, utc_timestamp);
+insert into restore (id, name, shipping_fee, kitchen_id, creation_date_time, updated_date_time) values (2, 'Thai Delivery', 9.50, 1, utc_timestamp, utc_timestamp);
+insert into restore (id, name, shipping_fee, kitchen_id, creation_date_time, updated_date_time) values (3, 'Tuk Tuk Comida Indiana', 15, 2, utc_timestamp, utc_timestamp);
 
 insert into state (id, name) values (1, 'Minas Gerais');
 insert into state (id, name) values (2, 'São Paulo');
